34e107f6c0bf05fff03d5984f01272c44e800b56
drkchiloll <sam.womack@entropicos.com>
2020-09-14 10:32:03 -0500

    added example for retrieving device(s)

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/ApogeeNetworking/rgnets"
@@ -21,7 +22,17 @@ func main() {
 	// leaseID := "1048577"
 	// rxg.DhcpRelease(leaseID)
 	// doptions := rgnets.DhcpLeaseOptions{IP: "ipAddr"}
-	doptions := rgnets.DhcpLeaseOptions{Hostname: "ap01.rXgCluster"}
-	dlease, _ := rxg.GetDhcpLease(doptions)
-	fmt.Println(dlease)
+	// doptions := rgnets.DhcpLeaseOptions{Hostname: "ap01.rXgCluster"}
+	// dlease, _ := rxg.GetDhcpLease(doptions)
+	// fmt.Println(dlease)
+	dops := rgnets.GetDeviceOptions{
+		// MacAddr: "78:45:61:c2:93:9f",
+		// DeviceID: 111,
+		AccountID: 88,
+	}
+	devs, e := rxg.GetDevice(dops)
+	if e != nil {
+		log.Fatalf("%v", e)
+	}
+	fmt.Println(devs)
 }
