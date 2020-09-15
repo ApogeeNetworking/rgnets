bd7686b89aacccaac516cf080953165a82e3ba2a
drkchiloll <sam.womack@entropicos.com>
2020-09-15 08:47:34 -0500

    create proper query string(s) for requests to query device(s)

--- devices.go
+++ devices.go
@@ -2,27 +2,32 @@ package rgnets
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"strconv"
 )
 
 // GetDevice ...
 func (c *Client) GetDevice(options GetDeviceOptions) ([]Device, error) {
 	var devices []Device
-	ep := fmt.Sprintf("/devices/index.json?api_key=%s", c.apiKey)
+	ep := "/devices/index.json"
 
-	switch {
-	case options.AccountID != 0:
-		ep += fmt.Sprintf("&account_id=%v", options.AccountID)
-	case options.DeviceID != 0:
-		ep += fmt.Sprintf("&id=%v", options.DeviceID)
-	case options.MacAddr != "":
-		ep += fmt.Sprintf("&mac=%s", options.MacAddr)
-	}
 	req, err := http.NewRequest("GET", c.BaseURL+ep, nil)
 	if err != nil {
 		return devices, err
 	}
+	// Create a Proper QueryString for REQ
+	qs := req.URL.Query()
+	qs.Add("api_key", c.apiKey)
+	switch {
+	case options.AccountID != 0:
+		qs.Add("account_id", strconv.Itoa(options.AccountID))
+	case options.DeviceID != 0:
+		qs.Add("id", strconv.Itoa(options.DeviceID))
+	case options.MacAddr != "":
+		qs.Add("mac", options.MacAddr)
+	}
+	// Add QueryString Encoded properly
+	req.URL.RawQuery = qs.Encode()
 	res, err := c.http.Do(req)
 	if err != nil {
 		return devices, err
