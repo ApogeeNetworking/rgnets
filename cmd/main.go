ba28a70084e77df124eacb2e1d1effff7206cc32
drkchiloll <sam.womack@entropicos.com>
2020-09-15 08:48:35 -0500

    update example request queries for the library

--- cmd/main.go
+++ cmd/main.go
@@ -19,12 +19,26 @@ func init() {
 
 func main() {
 	rxg := rgnets.New(host, apiKey, true)
-	// leaseID := "1048577"
-	// rxg.DhcpRelease(leaseID)
-	// doptions := rgnets.DhcpLeaseOptions{IP: "ipAddr"}
-	// doptions := rgnets.DhcpLeaseOptions{Hostname: "ap01.rXgCluster"}
-	// dlease, _ := rxg.GetDhcpLease(doptions)
-	// fmt.Println(dlease)
+	accountOptions := rgnets.GetAccountOptions{
+		ID: 88,
+	}
+	account, _ := rxg.GetAccount(accountOptions)
+	fmt.Println(account)
+}
+
+func dhcpOps(rxg *rgnets.Client) {
+	dlops := rgnets.DhcpLeaseOptions{
+		IP: "ipAddr",
+		// Hostname: "ap01.rXgCluster",
+	}
+	dlease, _ := rxg.GetDhcpLease(dlops)
+	fmt.Println(dlease)
+
+	// Release the DHCP Lease
+	rxg.ReleaseDHCP(dlease.ID)
+}
+
+func getDevice(rxg *rgnets.Client) {
 	dops := rgnets.GetDeviceOptions{
 		// MacAddr: "78:45:61:c2:93:9f",
 		// DeviceID: 111,
